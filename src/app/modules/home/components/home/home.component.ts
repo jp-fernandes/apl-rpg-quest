1b9e27e94b1f8584494e6b208a03b7255348498f
Joao Paulo Fernandes <joao.paulo.f.souza@accenture.com>
2023-06-15 16:08:26 -0300

    inclusao da home e ajustes de telas do app

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'rpg-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  name = 'Nome da Pessoa';
+  //To-Do
+  // 1 - Buscar o nome da pessoa, talvez ver local storage ou outra coisa.
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+  studyPaths(): void {
+    // Navegue para a página de trilhas para estudo
+    this.router.navigate(['/assuntos']);
+  }
+
+  takeExams(): void {
+    //To-Do
+    // 1 - Verificar se o usuário estudou algum conteúdo na página de "trilhas para estudo" - Só permitir fazer a prova se tiver feito isso.
+    this.router.navigate(['/provas']);
+  }
+
+  restartFromZero(): void {
+    //To-Do
+    // Lógica para reiniciar o jogo do zero
+    // perguntar se realmente quer de ctz reiniciar o jogo
+  }
+
+  viewCurrentPerformance(): void {
+    // Navegue para a página de desempenho atual
+    this.router.navigate(['/current-performance']);
+  }
+
+  viewProfile(): void {
+    // Navegue para a página de perfil
+    this.router.navigate(['/profile']);
+  }
+
+  viewGameRules(): void {
+    // Navegue para a página de regras do jogo
+    this.router.navigate(['/game-rules']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,9 @@ import { CreateProfileComponent } from './components/create-profile/create-profi
     CreateProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
