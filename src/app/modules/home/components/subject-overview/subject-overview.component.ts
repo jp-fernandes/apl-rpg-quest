5a08c8a1ca04ce93b1e93607a63791dfe74fb77a
Joao Paulo Fernandes <joao.paulo.f.souza@accenture.com>
2023-06-21 12:51:25 -0300

    nova pagina de overview

--- src/app/modules/home/components/subject-overview/subject-overview.component.ts
+++ src/app/modules/home/components/subject-overview/subject-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectService } from '../../services/subject.service';
 
 @Component({
   selector: 'app-subject-overview',
@@ -7,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectOverviewComponent implements OnInit {
   chosenOption!: string;
+  title!: string;
+  overview!: string;
+  subTopics: string[] = [];
 
-  constructor() { }
+  constructor(
+    private subjectService: SubjectService
+  ) { }
 
   ngOnInit(): void {
     this.chosenOption = window.history.state && window.history.state.chosenOption;
     console.log("opcao recebida ", this.chosenOption)
+
+    if (this.chosenOption) {
+      const subjectData = this.subjectService.getSubjectData(this.chosenOption);
+
+      if (subjectData) {
+        this.title = subjectData.title;
+        this.overview = subjectData.overview;
+        this.subTopics = subjectData.subTopics;
+      }
+    } else {
+      // Redirecionar para a página inicial se a opção escolhida não for válida
+      // ... faça a implementação adequada para redirecionar para a página inicial
+    }
 
     //To-do
     // 1 - Validar essa PROP this.chosenOption pq caso atualizem a pagina direcionar para a home.
---
+++ src/app/modules/home/services/subject.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+  getSubjectData(subject: string) {
+    switch (subject) {
+      case 'portuguese':
+        return {
+          title: 'Língua Portuguesa',
+          overview: 'A Língua Portuguesa é um idioma amplamente falado e escrito em diversos países ao redor do mundo. Ela desempenha um papel fundamental na comunicação, na literatura e na cultura. Nessa disciplina, vamos explorar as principais características da língua, a gramática, a ortografia e a interpretação de textos, desenvolvendo habilidades de escrita e comunicação efetiva.',
+          subTopics: [
+            'Gramática: Estudaremos as regras e estruturas da língua portuguesa, incluindo os tempos verbais, a concordância nominal e verbal, e a pontuação.',
+            'Ortografia: Exploraremos as regras ortográficas, incluindo o uso correto das letras, acentuação e pontuação.',
+            'Interpretação de Textos: Desenvolveremos habilidades de leitura crítica, compreensão e análise de diversos tipos de textos.',
+            'Redação: Aprenderemos técnicas e estratégias para produção de textos coesos, coerentes e bem estruturados.'
+          ]
+        };
+      case 'mathematics':
+        return {
+          title: 'Matemática',
+          overview: 'A matemática é uma disciplina fundamental que estuda números, quantidades, estruturas, espaço e mudanças. É uma das áreas mais antigas do conhecimento humano e desempenha um papel essencial em diversas áreas da ciência, tecnologia, engenharia e economia. Nessa matéria, vamos explorar os principais conceitos e tópicos matemáticos, desenvolvendo habilidades de raciocínio lógico e resolução de problemas.',
+          subTopics: [
+            'Equação de 1º Grau: Estudaremos como resolver e trabalhar com equações lineares de primeiro grau.',
+            'Teorema de Pitágoras: Exploraremos o famoso teorema que relaciona os lados de um triângulo retângulo.',
+            'Probabilidade: Introduziremos os conceitos básicos de probabilidade e sua aplicação em eventos aleatórios.',
+            'Cálculo de Áreas: Abordaremos os métodos para calcular áreas de figuras geométricas, como quadrados, retângulos, triângulos e círculos.'
+          ]
+        };
+      case 'sciences':
+        return {
+          title: 'Ciências Naturais',
+          overview: 'As Ciências Naturais são uma área do conhecimento que estuda os fenômenos naturais, incluindo os seres vivos, o meio ambiente, a Terra, o universo e suas interações. Nessa disciplina, vamos explorar os principais conceitos e tópicos das ciências naturais, desenvolvendo uma compreensão dos processos naturais e sua relevância para a vida e o mundo ao nosso redor.',
+          subTopics: [
+            'Biologia: Estudaremos os seres vivos, sua estrutura, função, classificação e interações com o ambiente.',
+            'Física: Exploraremos as leis fundamentais da física, como movimento, energia, eletricidade e magnetismo.',
+            'Química: Introduziremos os princípios básicos da química, como átomos, elementos, compostos e reações químicas.',
+            'Ecologia: Abordaremos as relações dos seres vivos com o meio ambiente e a importância da conservação e sustentabilidade.'
+          ]
+        };
+      case 'history':
+        return {
+          title: 'História',
+          overview: 'A História é a disciplina que estuda os eventos, pessoas, culturas e sociedades do passado. Ela nos ajuda a compreender como o mundo evoluiu ao longo do tempo e como os acontecimentos passados moldaram o presente. Nessa matéria, vamos explorar os principais períodos, acontecimentos e figuras históricas, desenvolvendo uma perspectiva histórica e uma apreciação pela diversidade cultural e social.',
+          subTopics: [
+            'Pré-História: Estudaremos os primeiros seres humanos, suas formas de vida, ferramentas e a transição para a vida em sociedade.',
+            'Idade Antiga: Exploraremos as civilizações antigas, como egípcios, gregos, romanos e suas contribuições para a história.',
+            'Idade Média: Analisaremos o período medieval, incluindo o feudalismo, as cruzadas, o renascimento comercial e cultural.',
+            'Idade Moderna e Contemporânea: Abordaremos os principais acontecimentos da Idade Moderna e Contemporânea, como as grandes navegações, a Revolução Industrial, as guerras mundiais e os movimentos sociais.'
+          ]
+        };
+      case 'geography':
+        return {
+          title: 'Geografia',
+          overview: 'A Geografia é uma disciplina que estuda o espaço, o ambiente e as relações entre as pessoas e o meio em que vivem. Ela nos ajuda a compreender os aspectos físicos, sociais, econômicos e culturais do mundo. Nessa matéria, vamos explorar os principais conceitos geográficos, como relevo, clima, população, economia e globalização, desenvolvendo uma compreensão das interações entre os lugares e as questões geográficas contemporâneas.',
+          subTopics: [
+            'Relevo e Hidrografia: Estudaremos as formas de relevo da Terra, como montanhas, planícies, rios e oceanos, e sua influência na distribuição dos seres vivos e das atividades humanas.',
+            'Clima e Vegetação: Exploraremos os diferentes tipos de clima e vegetação do planeta, suas características e influências na biodiversidade e nas atividades humanas.',
+            'População e Migração: Analisaremos os padrões de distribuição da população mundial, os movimentos migratórios e suas causas e consequências sociais, econômicas e culturais.',
+            'Economia e Globalização: Abordaremos os sistemas econômicos, as atividades produtivas, o comércio internacional e a interdependência global, destacando as desigualdades e os desafios socioambientais.'
+          ]
+        };
+      default:
+        return null;
+    }
+  }
+}
